b19f57be3626576c817abe6cb6d847bea5e5b5c8
purnika <purnika.kush41@gmail.com>
2018-10-25 18:30:48 +0530

    rectified chat forum

--- lib/ui/agro.dart
+++ lib/ui/agro.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:firebase_database/firebase_database.dart';
-import '../models/model.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
+import '../ui/chat.dart';
 
 class Agro extends StatefulWidget {
   @override
@@ -80,6 +78,7 @@ class _AgroState extends State<Agro> {
                         margin:
                             const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 170.0),
                         child: FloatingActionButton(
+                          heroTag: 'btn1',
                           onPressed: null,
                           child: new Icon(Icons.camera_alt),
                           backgroundColor: Colors.green.shade900,
@@ -89,7 +88,7 @@ class _AgroState extends State<Agro> {
                         alignment: Alignment.bottomRight,
                         margin: const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 30.0),
                         child: FloatingActionButton(
-                          heroTag: 'btn1',
+                          heroTag: 'btn2',
                           onPressed: () {
                             var router = new MaterialPageRoute(builder: (BuildContext context) {
                              return new chat();
@@ -105,6 +104,7 @@ class _AgroState extends State<Agro> {
                         margin:
                             const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 100.0),
                         child: FloatingActionButton(
+                            heroTag: 'btn3',
                           onPressed: () {
                             call();
                           },
@@ -135,122 +135,6 @@ class _AgroState extends State<Agro> {
         ));
   }
 }
-
-class chat extends StatefulWidget {
-  @override
-  _chatState createState() => _chatState();
-}
-
-class _chatState extends State<chat> {
-  List<Board> boardMessages = List();
-  Board board;
-  final FirebaseDatabase database = FirebaseDatabase.instance;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  DatabaseReference databseReference;
-
-  @override
-  void initState() {
-    super.initState();
-    board = new Board("", "");
-    databseReference = database.reference().child("community_board");
-    databseReference.onChildAdded.listen(_onEntryAdded);
-    databseReference.onChildChanged.listen(_onEntryChanged);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('AgroGo'),
-        backgroundColor: Colors.green.shade800,
-      ),
-
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 0,
-            child: Center(
-              child: Flex(
-                key: formKey,
-                direction: Axis.vertical,
-                children: <Widget>[
-                  ListTile(
-                    leading: Icon(Icons.subject),
-                    title: TextFormField(
-                      initialValue: "",
-                      onSaved: (val) => board.subject = val,
-                      validator: (val) => val == "" ? val : null,
-                    ),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.message),
-                    title: TextFormField(
-                      initialValue: "",
-                      onSaved: (val) => board.body = val,
-                      validator: (val) => val == "" ? val : null,
-                    ),
-                  ),
-                  FlatButton(
-                    child: Text("Post"),
-                    onPressed: () {
-                      handleSubmit();
-                    },
-                    color: Colors.green.shade800,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Flexible(
-            child: FirebaseAnimatedList(
-              query: databseReference,
-              itemBuilder: (_, DataSnapshot snapshot,
-                  Animation<double> animation, int index) {
-                return new Card(
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.green,
-                    ),
-                    title: Text(boardMessages[index].subject),
-                    subtitle: Text(boardMessages[index].body),
-                  ),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  void _onEntryAdded(Event event) {
-    setState(() {
-      boardMessages.add(Board.fromSnapshot(event.snapshot));
-    });
-  }
-
-  void handleSubmit() {
-    final FormState form = formKey.currentState;
-    if (form.validate()) {
-      form.save();
-      form.reset();
-
-      databseReference.push().set(board.toJson());
-    }
-  }
-
-  void _onEntryChanged(Event event) {
-    var oldEntry = boardMessages.singleWhere((entry){
-      return entry.key == event.snapshot.key;
-    });
-
-    setState(() {
-      boardMessages[boardMessages.indexOf(oldEntry)] =
-          Board.fromSnapshot(event.snapshot);
-    });
-  }
-}
-
 
 call() async {
   await launch('tel:18001801551');
